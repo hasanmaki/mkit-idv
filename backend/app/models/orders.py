49fce9a095380ae379cee879c66b646299bac1f9
Kafka Hasan Maki <182825954+hasanmaki@users.noreply.github.com>
2026-02-08 21:49:35 +0700

    refactor(customers): remove customer model and add orders model

---
+++ backend/app/models/orders.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2026 okedigitalmedia/hasanmaki. All rights reserved.
+# [ ] TODO : Fix Later About Docstring
+"""orders and related models.
+
+Short description of this module and its responsibilities. Explain its purpose within the application architecture.
+
+Key Features:
+    - First key feature
+    - Second key feature
+
+Attributes:
+    - Second key feature
+    - Second key feature
+
+Example:
+    from module import something
+
+Note:
+    - Important constraints or considerations
+"""
